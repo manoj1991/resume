2e93fa9200657e32048ac8b188b406384da2e356
manoj1991 <manojmkadolkar@gmail.com>
2017-01-16 11:22:11 +0530

    Initial commit

---
+++ js/chow-custom.js
@@ -0,0 +1,40 @@
+// CHOW-CUSTOM JS FILE STARTS
+$(document).ready(function(){
+	$("#progress-bar1").animate({
+    	width: "50%"
+	}, 2500);
+	$("#progress-bar2").animate({
+    	width: "60%"
+	}, 3000);
+	$("#progress-bar3").animate({
+    	width: "55%"
+	}, 3500);
+	$("#progress-bar4").animate({
+    	width: "80%"
+	}, 4000);
+	$("#progress-bar5").animate({
+    	width: "70%"
+	}, 4500);
+	$("#progress-bar6").animate({
+    	width: "90%"
+	}, 5000);
+	$("#progress-bar7").animate({
+    	width: "80%"
+	}, 5500);
+	$("#progress-bar8").animate({
+    	width: "65%"
+	}, 6000);
+	$("#progress-bar9").animate({
+    	width: "75%"
+	}, 6500);
+	$("#progress-bar10").animate({
+    	width: "75%"
+	}, 7000);
+	$("#progress-bar11").animate({
+    	width: "100%"
+	}, 7500);
+	$("#progress-bar12").animate({
+    	width: "95%"
+	}, 8000);
+})
+// CHOW-CUSTOM JS FILE ENDS
